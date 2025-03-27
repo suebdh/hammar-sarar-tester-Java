97b4246d46e2df16e0dcfedb7c74e62a8612db03
Sarar Hammar <hammar.sarar@gmail.com>
2025-03-27 18:20:33 +0100

    Update the calculateFare method in the FareCalculatorService class to return a price of 0 if the parking duration is less than 30 minutes

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -16,6 +16,12 @@ public class FareCalculatorService {
         //TODO: Some tests are failing here. Need to check if this logic is correct
         double duration = (outHour - inHour) /(60.0*60.0*1000.0);
 
+        // Si la durée est inférieure à 30 minutes, le prix doit être égal à 0
+        if (duration < 0.5) {
+            ticket.setPrice(0);
+            return; // On quitte la méthode, pas d'autre calcul à effectuer
+        }
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
